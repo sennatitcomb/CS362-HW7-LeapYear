5e4697cab72bca0e81641c9db8048d43fb9af3fa
Senna <sennatitcomb@gmail.com>
2021-05-28 12:07:49 -0700

    implemented integrated if else statement and modulus operator

--- test_leapyear.py
+++ test_leapyear.py
@@ -2,8 +2,8 @@ import unittest
 import leapyear
 
 class TestCase(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(leapyear.leapyear(200), "True")
+    #def test1(self):
+     #   self.assertEqual(leapyear.leapyear(200), "True")
     def test2(self):
         self.assertEqual(leapyear.leapyear(2000), "False")
 
