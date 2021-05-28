f2f51573d5815b1cbe1a04c7c07e18dc6d7b747b
Senna <sennatitcomb@gmail.com>
2021-05-28 12:12:36 -0700

    Wrote 4th test, checks to see if entering a valid leap year return string "This is a leap year" isntead of string "This is not a leap year"

--- test_leapyear.py
+++ test_leapyear.py
@@ -6,8 +6,10 @@ class TestCase(unittest.TestCase):
      #   self.assertEqual(leapyear.leapyear(200), "True")
     #def test2(self):
      #   self.assertEqual(leapyear.leapyear(2000), "False")
-    def test3(self):
-        self.assertEqual(leapyear.leapyear(2000), "True")
+    #def test3(self):
+     #   self.assertEqual(leapyear.leapyear(2000), "True")
+    def test4(self):
+        self.assertEqual(leapyear.leapyear(2000), "This is a leap year")
 
 if __name__ == '__main__':
     unittest.main()
