7d4340d0736cfec86112472cb041cb45d27da9f8
Senna <sennatitcomb@gmail.com>
2021-05-28 12:05:43 -0700

    Wrote 2nd test, tests if year is evenly divisible by 4 and by 100, if yes then return False

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,7 +4,8 @@ import leapyear
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapyear.leapyear(200), "True")
-   
+    def test2(self):
+        self.assertEqual(leapyear.leapyear(2000), "False")
 
 if __name__ == '__main__':
     unittest.main()
