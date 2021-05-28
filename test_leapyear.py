c3b6a15dd1f647528df34dc5367eb722906a7d15
Senna <sennatitcomb@gmail.com>
2021-05-28 12:10:08 -0700

    Wrote 3rd test, tests if number is evenly divisible by 4, divisble by 100, and divisible by 400, returns True if yes and False if no

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,8 +4,10 @@ import leapyear
 class TestCase(unittest.TestCase):
     #def test1(self):
      #   self.assertEqual(leapyear.leapyear(200), "True")
-    def test2(self):
-        self.assertEqual(leapyear.leapyear(2000), "False")
+    #def test2(self):
+     #   self.assertEqual(leapyear.leapyear(2000), "False")
+    def test3(self):
+        self.assertEqual(leapyear.leapyear(2000), "True")
 
 if __name__ == '__main__':
     unittest.main()
